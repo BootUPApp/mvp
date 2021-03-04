b78f5d8f64c88324246c0e5895d4087d9739ded2
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-03-04 18:07:06 +0100

    Recruiter Routes Server Ready

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,32 @@
 const router = require("express").Router();
 const Recruiter = require("../models/Recruiter");
 
-// Get Recruiter / Insomnia Test x
+// GET Recruiter / Insomnia Test x
 router.get("/recruiter", (req,res, next) => {
-  Recruiter.find().then(recruiterFromDB => {
-      res.json(recruiterFromDB);
+  Recruiter.find().then(recruitersFromDB => {
+      res.json(recruitersFromDB);
   }).catch(error => {
     next(error);
   })
 })
 
+// GET Specifc Recruiter / Insomnia Test x
+router.get("/recruiter/:id", (req,res, next) => {
+  Recruiter.findById(req.params.id).then((selectedRecruiterfromDB) => {
+      res.status(200).json(selectedRecruiterfromDB);
+  }).catch(error => {
+    next(error);
+  })
+})
 
-// Post Recruiter / Insomnia Test x
+// POST Recruiter / Insomnia Test x
 router.post("/recruiter", (req, res, next) => {
   Recruiter.create({
      firstName: req.body.firstName,
      secondName: req.body.secondName,
      profileImage: req.body.profileImage, // Attention needs to be req.file due to multer/Cloudniary
      emailAddress: req.body.emailAddress,
+     companyName: req.body.companyName,
      // challenges: //
      // inbox:
   }).then(recruiterToDB => {
@@ -26,6 +35,32 @@ router.post("/recruiter", (req, res, next) => {
     next(error);
   })
 });
+
+// PUT Recruiter  - Update Profile / Insomnia Test x
+router.put("/recruiter/:id", (req,res,next) => {
+  Recruiter.findByIdAndUpdate(req.params.id, {
+    firstName: req.body.firstName,
+    secondName: req.body.secondName,
+    profileImage: req.body.profileImage, // Attention needs to be req.file due to multer/Cloudniary
+    emailAddress: req.body.emailAddress,
+    companyName: req.body.companyName, 
+  }, 
+  {new: true})
+  .then(recruitersFromDB => {
+    res.status(200).json(recruitersFromDB)
+  }).catch(error => {
+      next(error);
+  })
+})
+
+// DELETE Recruiter 
+router.delete("/recruiter/:id", (req,res,next) => {
+  Recruiter.findByIdAndDelete(req.params.id).then(deletedRecruiter => {
+      res.status(200).json({message: 'Profile Succesfully deleted'})
+  }).catch(error => {
+    next(error);
+  })
+})
 
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
