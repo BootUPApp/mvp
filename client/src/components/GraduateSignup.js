725cd7d459cbb214af6d7c5a1c522387e7cad2d1
Simon Gowing <simongowing1@gmail.com>
2021-03-09 12:50:04 +0100

    lunch time commit

--- client/src/components/GraduateSignup.js
+++ client/src/components/GraduateSignup.js
@@ -123,6 +123,7 @@ class GraduateSignup extends React.Component{
           // we want to put the user object in the state of App.js
           console.log(user);
           this.setState({message: ''})
+          this.props.history.push(`/graduates`)
         }
       })
   }
